0dde1744df2359de2a2894e5acdbcca57a810ddc
Pouya <pouyajoon@gmail.com>
2014-08-17 14:59:06 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaarchi::Application.config.secret_key_base = '372b53bce3c4792edbc605749448d44c004cdece98b5b26492cc60e3342de305c27c4f7faba9b89c57a2d30316872bf702990a393689b941f12a534a3136f408'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaarchi::Application.config.session_store :cookie_store, key: '_kaarchi_session'
